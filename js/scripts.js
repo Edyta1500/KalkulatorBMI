7b3196ec5cc29d9b4069d5fe156e557b28091239
Edyta Dudek <edyta.dudek@10g.pl>
2022-09-03 00:02:23 +0200

    calculator works

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,40 @@
 'use strict';
 
 const btn = document.querySelector('button');
-const weight = document.querySelector('#weight').value;
-const height = document.querySelector('#height').value;
 
+const resPlace = document.querySelector('.result');
+const interprText = document.querySelector('.interpretation');
+const interpr = ["wygłodzenie","wychudzenie", "niedowaga", "waga prawidłowa", "nadwaga", "otyłość I-go stopnia", "otyłość II-go stopnia", "otyłość II-go stopnia"];
 
-const calc = () =>{
-    const resPlace = document.querySelector('.result');
-    const result = Math.round(weight/(Math.pow((height/100),2)));
-    const interpr = ["wygłodzenie","wychudzenie", "niedowaga", "waga prawidłowa", "nadwaga", "otyłość I-go stopnia", "otyłość II-go stopnia", "otyłość II-go stopnia"];
     
+    
+    
+
+
+
+btn.addEventListener('click', ()=>{
+    const weight = document.querySelector('#weight').value;
+    const height = document.querySelector('#height').value;
+    const result = (Math.round(parseInt(weight)/Math.pow((parseInt(height)*.01),2),(2)));
+console.log(result);
+    resPlace.innerHTML = result;
     if (result<16){
-        resPlace.innerHTML= interpr[0];
+        interprText.innerHTML= interpr[0];
     }else if(result<17){
-        resPlace.innerHTML = interpr[1];
+        interprText.innerHTML = interpr[1];
     }else if(result<18.5){
-        resPlace.innerHTML = interpr[2];
+        interprText.innerHTML = interpr[2];
     }else if(result<24.5){
-        resPlace.innerHTML = interpr[3];
+        interprText.innerHTML = interpr[3];
     }else if(result<30){
-        resPlace.innerHTML = interpr[4];
+        interprText.innerHTML = interpr[4];
     }else if(result<35){
-        resPlace.innerHTML = interpr[5];
+        interprText.innerHTML = interpr[5];
     }else if(result<40){
-        resPlace.innerHTM = interpr[6];
+        interprText.innerHTM = interpr[6];
     }else 
         resPlace.innerHTML = '';
 
+});
 
-}
-btn.addEventListener('click', calc);
+
