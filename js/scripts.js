a59a6b826598424f398ae6e9b8959152d46befd0
Edyta Dudek <edyta.dudek@10g.pl>
2022-09-04 16:41:49 +0200

    p wtd added

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,56 @@
 
 const btnCalc = document.querySelector('.calculate_button');
 
-const btnRes = document.querySelector('.reset_button');
+const btnReset = document.querySelector('.reset_button');
 
-const resPlace = document.querySelector('.result');
+const resultPlace = document.querySelector('.result');
 
 const interprText = document.querySelector('.interpretation');
 
 const interpr = ["wygłodzenie", "wychudzenie", "niedowaga", "waga prawidłowa", "nadwaga", "otyłość I-go stopnia", "otyłość II-go stopnia", "otyłość II-go stopnia"]
 
-btnCalc.addEventListener('click', () => {
-    
-    const weight = document.querySelector('#weight').value;
-    
-    const height = document.querySelector('#height').value;
-    
-    const result = (Math.round(parseInt(weight) / Math.pow((parseInt(height) * .01), 2), (2)));
-    
-    resPlace.innerHTML = result;
-    
-    if (result < 16) {
-        interprText.innerHTML = interpr[0];
-    } else if (result < 17) {
-        interprText.innerHTML = interpr[1];
-    } else if (result < 18.5) {
-        interprText.innerHTML = interpr[2];
-    } else if (result < 24.5) {
-        interprText.innerHTML = interpr[3];
-    } else if (result < 30) {
-        interprText.innerHTML = interpr[4];
-    } else if (result < 35) {
-        interprText.innerHTML = interpr[5];
-    } else if (result < 40) {
-        interprText.innerHTM = interpr[6];
-    } else
-        resPlace.innerHTML = '';
+const calc = ()=>{
 
-});
+const weight = document.querySelector('#weight');
 
-btnRes.addEventListener('click', ()=>{
+const height = document.querySelector('#height');
 
+const result = (Math.round(parseInt(weight.value) / Math.pow((parseInt(height.value) * .01), 2), (2)));
+
+
+resultPlace.innerHTML = result;
     
-    resPlace.innerHTML = '';
+if (result < 16) {
+    interprText.innerHTML = interpr[0];
+} else if (result < 17) {
+    interprText.innerHTML = interpr[1];
+} else if (result < 18.5) {
+    interprText.innerHTML = interpr[2];
+} else if (result < 24.5) {
+    interprText.innerHTML = interpr[3];
+} else if (result < 30) {
+    interprText.innerHTML = interpr[4];
+} else if (result < 35) {
+    interprText.innerHTML = interpr[5];
+} else if (result < 40) {
+    interprText.innerHTM = interpr[6];
+} else
+    resultPlace.innerHTML = '';
+
+}
+
+btnCalc.addEventListener('click', calc )
+
+btnReset.addEventListener('click', ()=>{
+   
+    
+    resultPlace.innerHTML = '';
     
     interprText.innerHTML = '';
+
+    weight.value= '';
+
+    height.value = '';
 })
 
 
