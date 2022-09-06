540672a1d86b35ce93075e465d38055383521b1c
Edyta Dudek <edyta.dudek@10g.pl>
2022-09-06 11:58:48 +0200

    js finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//Zmienne
 const btnCalc = document.querySelector('.calculate_button');
 
 const btnReset = document.querySelector('.reset_button');
@@ -9,61 +9,43 @@ const resultPlace = document.querySelector('.result');
 
 const interprText = document.querySelector('.interpretation');
 
-const interpr = ["wygłodzenie", "wychudzenie", "niedowaga", "waga prawidłowa", "nadwaga", "otyłość I-go stopnia", "otyłość II-go stopnia", "otyłość III-go stopnia"]
-
 const wtdBox = document.querySelectorAll('.wtd__box');
 
+const weight = document.querySelector('#weight');
+
+const height = document.querySelector('#height');
+
+//tablice
+
+const interpr = ["wygłodzenie", "wychudzenie", "niedowaga", "waga prawidłowa", "nadwaga", "otyłość I-go stopnia", "otyłość II-go stopnia", "otyłość III-go stopnia"]
+
+const points = [0, 16, 17, 18.5, 24.5, 30, 35, 40, 200];
+
+//funkcje
+
 const calc = () => {
-
-    const weight = document.querySelector('#weight');
-
-    const height = document.querySelector('#height');
 
     const result = (Math.round(parseInt(weight.value) / Math.pow((parseInt(height.value) * .01), 2), (2)));
 
     resultPlace.innerHTML = result;
 
+    for (let i = 0; i < points.length - 1; i++) {
+        
+        if (result >= points[i] && result < points[i + 1]) {
+           
+            interprText.innerHTML = interpr[i];
+            
+            wtdBox[i].classList.remove('disp-none');
 
-    for (let i = 0; i <= wtdBox.length - 1; i++) {
-
-
-        if (i == 0 && result < 16 && result >= 0) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 1 && result >= 16 && result < 17) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 2 && result >= 17 && result < 18.5) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 3 && result >= 18.5 && result < 24.5) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 4 && result >= 24.5 && result < 30) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 5 && result >= 30 && result < 35) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 6 && result >= 35 && result < 40) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
-        } else if (i == 7 && result >= 40) {
-            interprText.innerHTML = interpr[i];
-            wtdBox[i].classList.remove('disp-none');
         } else {
+
             wtdBox[i].classList.add('disp-none');
         }
-
     }
+
 }
 
-
-
-btnCalc.addEventListener('click', calc)
-
-btnReset.addEventListener('click', () => {
-
+const reset = () => {
 
     resultPlace.innerHTML = '';
 
@@ -74,24 +56,31 @@ btnReset.addEventListener('click', () => {
     height.value = '';
 
     for (let i = 0; i <= wtdBox.length - 1; i++) {
-        wtdBox[i].classList.add('disp-none')
+       
+        wtdBox[i].classList.add('disp-none');
     }
+}
 
 
-})
-
-const clear = ()=>{
+const clear = () => {
+   
     resultPlace.innerHTML = '';
-    interprText.innerHTML ='';
+   
+    interprText.innerHTML = '';
+   
     for (let i = 0; i <= wtdBox.length - 1; i++) {
-        wtdBox[i].classList.add('disp-none')
+       
+        wtdBox[i].classList.add('disp-none');
     }
-
 
 };
+//nasłuchiwacze zdarzeń
+
+btnCalc.addEventListener('click', calc);
+
+btnReset.addEventListener('click', reset);
 
 weight.addEventListener('input', clear);
-
 
 height.addEventListener('input', clear);
 
